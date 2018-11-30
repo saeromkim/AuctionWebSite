0eb1f67270ef5be092c977df15bf6d0b98177a83
yeonggilee <kwa03065@gmail.com>
2018-12-01 00:54:45 +0900

    first commit

--- ssido-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ ssido-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
 import Header from 'components/common/Header';
+<<<<<<< HEAD
 import Menu from 'components/common/menu/Menu';
 import Footer from 'components/common/Footer';
+=======
+>>>>>>> first commit
 
 const cx=classNames.bind(styles);
 
@@ -11,11 +14,17 @@ const PageTemplate = ({children}) => {
     return (
         <div className={cx('page-template')}>
             <Header/>
+<<<<<<< HEAD
             <Menu/>
             <main>
                 {children}
             </main>
             <Footer/>
+=======
+			<main>
+				{children}
+			</main>
+>>>>>>> first commit
         </div>
     );
 };
