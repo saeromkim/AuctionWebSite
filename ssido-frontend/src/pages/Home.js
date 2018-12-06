eb0aa3f7b37e6a7a37606ba061e8b1916510f139
saeromkim247 <ksr24777@gmail.com>
2018-12-06 21:10:32 +0900

    RoomList 꾸미기

--- ssido-frontend/src/pages/Home.js
+++ ssido-frontend/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import Event from 'components/Event';
+import RoomList from 'components/RoomList';
 
 const Home = () => {
     return (
         <div>
             <PageTemplate>
                 <Event/>
+                <RoomList/>
             </PageTemplate>
         </div>
     );
