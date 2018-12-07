5ac3bec0c689d0c553859de67085bebcee629a91
saeromkim247 <ksr24777@gmail.com>
2018-12-07 14:24:35 +0900

    merge master<-form

--- ssido-frontend/src/pages/index.js
+++ ssido-frontend/src/pages/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from './Home';
 export { default as NotFoundPage } from './NotFoundPage';
-export { default as Form } from './form';
+export { default as Form } from './Form';
